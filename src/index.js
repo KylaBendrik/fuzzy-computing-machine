c498bc04708327d38df6721ac7390446ce48a885
Beth Ashton <Mbeth.ashton@gmail.com>
2020-02-11 14:54:31 -0500

    Make some changes to the menu buttons
    Close new Employee Wizard when you click the Dashboard button

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const createWindow = () => {
       nodeIntegration: true
     },
     width: 800,
-    height: 600,
+    height: 600
   });
 
   // and load the index.html of the app.
--- src/scripts/sidenav.js
+++ src/scripts/sidenav.js
@@ -1,44 +1,105 @@
-console.log('sidenav.js loaded');
-if (document != null){
-  console.log('document loaded')
-}
-
-console.log(require)
-if (require == null){
-  console.log("require is null")
-}
-if (require == undefined){
-  console.log("require is undefined")
-}
-if (require == false){
-  console.log("require is false")
-}
-// if (require != undefined){
-//   import { remote } from 'electron';
-//   const { BrowserWindow } = remote;
-// } else {
-  const { BrowserWindow } = require('electron').remote
-// }
-
-
-
-
+const { BrowserWindow } = require('electron').remote
 
 window.onload=function(){
-  const newEmployeeBtn = document.getElementById("new_employee");
+  //make this automated soon - used just for clicky bit
+  // const newEmployeeBtn = document.getElementById("new_employee_btn");
 
-  newEmployeeBtn.addEventListener('click', (event) => {
-    console.log('button pushed!')
-    let win = new BrowserWindow({ width: 600, height: 500, webPreferences: {
-      nodeIntegration: true
-    },})
-  
-    win.on('close', () => {win = null})
-    win.loadURL(`file://${__dirname}/new_employee_wizard.html`)
-    win.show()
-    // Open the DevTools.
-    win.webContents.openDevTools();
+  //console title
+  const side_nav_pages = [
+    {title_id: 'dashboard_page', 
+      button: 'dashboard_btn', 
+      html: 'index.html', 
+      text: 'Dashboard', 
+      color: '#007C89',
+      button_style: 'focus'
+    },
+    {title_id: 'new_employee_page', 
+      button: 'new_employee_btn',
+      html: 'new_employee_wizard.html', 
+      text: 'New Employee',
+      color: '#29712A',
+      button_style: 'new'
+    }
+  ]
+
+  // generate buttons
+  parentDiv = document.getElementById("sidenav")
+
+  side_nav_pages.forEach(function (page, index) {
+    element = document.createElement('button')
+    element.id = page.button
+    element.style.backgroundColor = page.color
+    element.innerHTML = page.text
+
+    // add event listener
+    element.addEventListener('click', (event) => {
+      console.log('button pushed: ', page.button)
+
+      if (page.button_style == 'new'){
+        let win = new BrowserWindow({ width: 600, height: 500, webPreferences: {
+          nodeIntegration: true
+        },})
+      
+        win.on('close', () => {win = null})
+        win.loadURL(`file://${__dirname}/${page.html}`)
+        win.show()
+    
+        // win.webContents.getElementById("new_employee").style.backgroundColor = 'black';
+        // Open the DevTools.
+        // win.webContents.openDevTools();
+      } else {
+        // focus on open page
+        console.log(BrowserWindow.getAllWindows())
+        window.close()
+      }
+      
+    })
+
+    parentDiv.appendChild(element)
   })
+
+
+  // find current page
+  const currentPage_id = document.getElementsByTagName("title")[0].id
+  console.log(currentPage_id)
+
+  function isCurrentPage(page) {
+    return page.title_id === currentPage_id
+  }
+
+  currentPage = side_nav_pages.find(isCurrentPage)
+
+  pageButton = document.getElementById(currentPage.button)
+
+  pageButton.style.left = '-100px'
+  pageButton.addEventListener("mouseover", mOver)
+  pageButton.addEventListener("mouseout", mOut)
+  pageButton.removeEventListener('click')
+
+  function mOver() {
+    pageButton.style.left = '0px'
+  }
+
+  function mOut() {
+    pageButton.style.left = '-100px'
+  }
+
+  // what happens when we click the button?
+
+  // newEmployeeBtn.addEventListener('click', (event) => {
+  //   console.log('button pushed!')
+  //   let win = new BrowserWindow({ width: 600, height: 500, webPreferences: {
+  //     nodeIntegration: true
+  //   },})
+  
+  //   win.on('close', () => {win = null})
+  //   win.loadURL(`file://${__dirname}/new_employee_wizard.html`)
+  //   win.show()
+
+  //   // win.webContents.getElementById("new_employee").style.backgroundColor = 'black';
+  //   // Open the DevTools.
+  //   win.webContents.openDevTools();
+  // })
 }
 
 
