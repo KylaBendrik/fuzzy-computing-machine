933bdfa434899303deb9ce767602d94d064e4c72
Beth Ashton <Mbeth.ashton@gmail.com>
2020-02-12 17:20:47 -0500

    Initial commit

---
+++ webpack.renderer.config.js
@@ -0,0 +1,20 @@
+const rules = require('./webpack.rules');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  }
+};
