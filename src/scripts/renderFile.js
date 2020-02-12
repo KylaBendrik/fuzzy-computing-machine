ebaff12c96964da8a35f0efe0077f7568ed45733
Beth Ashton <Mbeth.ashton@gmail.com>
2020-02-12 14:59:14 -0500

    Start fresh

--- src/scripts/renderFile.js
+++
@@ -1,9 +0,0 @@
-const ipcRenderer = require('electron').ipcRenderer;
-
-function sendForm(event) {
-    event.preventDefault() // stop the form from submitting
-    let firstname = document.getElementById("firstname").value;
-    let middlename = document.getElementById("middlename").value;
-    let lastname = document.getElementById('lastname').value;
-    ipcRenderer.send('form-submission', `${lastname}, ${firstname} ${middlename}`)
-}
