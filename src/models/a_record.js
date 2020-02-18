fd04724ab8c82034c4a74c08594fe7c25033c7bf
Beth Jackson <Mbeth.ashton@gmail.com>
2020-02-18 16:13:40 -0500

    Complete Goals
    Import contact info, spit out string

--- src/models/a_record.js
+++ src/models/a_record.js
@@ -12,23 +12,44 @@
 //   fax: '111-111-1111'
 // }
 let a_record = {
-  record_type: 'A',
-  contact_info: {
-    name: 'unknown',
-    email: 'unknown',
-    phone: 'unknown',
-    fax: 'unknown'
-  }
+  record_type: {value: 'A', start: 1, size: 1},
+  contact_info_name: {value: 'unknown', start: 73, size: 40},
+  contact_info_phone: {value: 'unknown', start: 113, size: 25},
+  contact_info_email: {value: 'unknown', start: 138, size: 25},
+  contact_info_fax: {value: 'unknown', start: 314, size: 25}
 }
 
 module.exports = {
   checkRecordType() {
-    return a_record.record_type;
+    return a_record.record_type.value;
   },
   updateContactInfo(contact_hash) {
-    for (const property in contact_hash) {
-      a_record.contact_info[property] = contact_hash[property]
-    }
+    a_record.contact_info_name.value  = contact_hash.name;
+    a_record.contact_info_email.value = contact_hash.email;
+    a_record.contact_info_phone.value = contact_hash.phone;
+    a_record.contact_info_fax.value   = contact_hash.fax;
+
     return a_record
+  },
+  exportARecord(record){
+    let record_array = []
+    for (let step = 0; step < 340; step ++) {
+      record_array.push(" ")
+    }
+
+    // add info from a_record
+    for (const prop of Object.values(record)) {
+      value = prop.value;
+      // size = prop.size; not sure we need here?
+      start = prop.start - 1;
+      array = prop.value.split('')
+      length = array.length;
+
+      for (let step = 0; step < length; step ++) {
+        record_array[start + step] = array[step]
+      }
+    }
+    
+  return record_array.join('')
   }
 };
