5fb3ea5d9cab484351db3fda08b91b0d259220ee
Beth Jackson <Mbeth.ashton@gmail.com>
2020-03-17 12:27:57 -0400

    Simple UI Progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,40 @@
-Hello from App!
+<script>
+  import Dashboard from "./views/dashboard.svelte";
+
+  let selected = {
+    component: Dashboard,
+    title: "Dashboard"
+  }
+
+  const pages = { Dashboard };
+
+  function handleMessage(event) {
+    console.log('handleMessage')
+    selected.component = pages[event.detail.destination];
+    selected.title = event.detail.title;
+    console.log(selected.title)
+  }
+  
+</script>
+
+<style type="text/scss">
+  @import "src/style/template.scss";
+</style>
+
+<main>
+  <header>{selected.title}</header>
+  <nav>
+    <ol>
+      <li><a href="dashboard">Dashboard</a></li>
+      <li><a href="employees">Employees</a></li>
+      <li><a href="export">Export</a></li>
+      <li><a href="orginfo">Organization</a></li>
+      <li><a href="myinfo">My Info</a></li>
+    </ol>
+  
+  </nav>
+  <article>
+    <svelte:component this={selected.component}/>
+  </article>
+</main>
+      
---
+++ src/views/dashboard.svelte
@@ -0,0 +1,38 @@
+
+<style type="text/scss">
+  $base_color: #007681;
+  .container {
+    display: grid;
+    grid-template-areas: "option1 option2"
+                      "option3 option4";
+  
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+
+    width: 100%;
+    height: 100%;
+  }
+  .option1 {
+    grid-area: option1;
+    background: $base_color
+  }
+  .option2 {
+    grid-area: option2;
+    background: mix(black, $base_color, 25%);
+  }
+  .option3 {
+    grid-area: option3;
+    background: mix(white, $base_color, 25%);
+  }
+  .option4 {
+    grid-area: option4;
+    background: mix(black, $base_color, 50%);
+  }
+</style>
+
+<div class="container">
+  <div class="option1"></div>
+  <div class="option2"></div>
+  <div class="option3"></div>
+  <div class="option4"></div>
+</div>
