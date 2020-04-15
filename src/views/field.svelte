bf6a89b4f66ba86f58f6a1402a1c7d8555f2a7d4
Beth Jackson <Mbeth.ashton@gmail.com>
2020-04-15 18:06:39 -0400

    Almost got employee records fixed.

---
+++ src/views/field.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let field;
+	export let value="";
+</script>
+<style type="text/scss">
+@import "src/style/form.scss";
+</style>
+<div class="tooltip">
+	<span class="tooltiptext">{field.description}</span>
+		<label for={field.value}>{field.field_label}</label>
+	{#if field.input_type == "radio"}
+		<ul>
+			{#each field.options as option}
+			<li>
+				<label for={option.value}>{option.label}</label>
+				<input type="radio" id={option.value} name={field.field_id} value={option.value} required={field.required} bind:group={value}>
+			</li>
+			{/each}
+		</ul>
+	{/if}
+	{#if field.input_type == "text"}
+		<input type="text" maxlength={field.size} required={field.required} bind:value={value}>
+	{/if}
+	{#if field.input_type == "number"}
+		<input type="text" maxlength={field.size} required={field.required} bind:value={value}>
+	{/if}
+	{#if field.input_type == "date"}
+		<input type="date" required={field.required} bind:value={value}>
+	{/if}
+</div>
--- src/views/view_employee.svelte
+++ src/views/view_employee.svelte
@@ -1,108 +1,116 @@
 <script>
-import { page } from '../stores.js';
-import { get } from 'svelte/store';
-import { c_record_fields } from '../c_record_fields.js';
+  import { page } from '../stores.js';
+  import { get } from 'svelte/store';
+  import all_fields from './data/c_fields.json'
+  import Field from './field.svelte'
 
-let all_fields = c_record_fields;
-
-let personal_fields = [ all_fields[1], all_fields[8],
-                        all_fields[2], all_fields[9],
-                        all_fields[3], all_fields[10],
-                        all_fields[4], all_fields[11],
-                        all_fields[5], all_fields[12],
-                        all_fields[6],
-                        all_fields[7],
-                        all_fields[17],
-                        all_fields[19], all_fields[26], all_fields[27]
-                        ]
-let employment_fields = [ all_fields[13], all_fields[14],
-                          all_fields[15],all_fields[16],
-                          all_fields[18],all_fields[20],
-                          all_fields[21],all_fields[23],
-                          all_fields[24],all_fields[25],]
-
-personal_fields.sort((a, b) => (a.start > b.start) ? 1: -1)
-
-let employee = {}
-
-var field;
-
-let value = get(page)
-
-if (value.params == undefined || value.params == {}){
-  console.log('employee blank')
-  for (field of all_fields){
-    employee[field.field_name] = field.default
-  }
+  // Generate forms
+	let personal_fields = []
+	let employment_fields = []
+	let payroll_fields = []
+	
+	all_fields.forEach(function(field){
+		if(field.form == "personal"){
+			personal_fields.push(field);
+		} else if (field.form == "employment"){
+			employment_fields.push(field)
+		} else {
+			payroll_fields.push(field)
+		}
+  })
   
-} else {
-  employee = value.params
-  console.log(JSON.stringify(employee))
-}
+  // Populate forms
+  let employee = {}
 
-function save() {
+  let page_value = get(page)
+
+  if (page_value.params == undefined || page_value.params == {}){
+    console.log('employee blank')
+    for (field of all_fields){
+      employee[field.field_id] = ""
+    }
+  } else {
+    employee = page_value.params
+    console.log(JSON.stringify(employee))
+  }
+
+  function save() {
     console.log("save()")
     MainAPI
     .saveData('employees', employee)
     .then(updatePageNum('3', {}))
-    //.then(updateStatus)
-}
+  } 
 
-function updatePageNum(new_id, params){
-  page.set({id: new_id, params: params})
-}
+  function updatePageNum(new_id, params){
+    page.set({id: new_id, params: params})
+  }
 
-	let personalVisible = true;
-	let payrollVisible = false;
-	
-	function togglePages(){
-		if(personalVisible == true){
-			personalVisible = false
-			payrollVisible = true
-		} else {
-			personalVisible = true
-			payrollVisible = false
-		}
+  // Tabs
+	let pages_visible = {
+		personal: true,
+		employment: false,
+		payroll: false
+	}
+	function pick_personal(){
+		pages_visible.personal = true;
+		pages_visible.employment = false;
+		pages_visible.payroll = false;
+	}
+		function pick_employment(){
+		pages_visible.personal = false;
+		pages_visible.employment = true;
+		pages_visible.payroll = false;
+	}
+		function pick_payroll(){
+		pages_visible.personal = false;
+		pages_visible.employment = false;
+		pages_visible.payroll = true;
 	}
 </script>
 <style type="text/scss">
   @import "src/style/form.scss";
+
+  .tabs button {
+		width: 150px;
+		border: none;
+		background: lightblue
+	}
 </style>
-<div class="field">
-  <button on:click={togglePages}>
-	{#if personalVisible}
-	  Employment
-	{/if}
-	{#if payrollVisible}
-	  Personal
-	{/if}
-</button>
-<button on:click={save}>Save</button>
+<div class="tabs">
+	<button id="personal_button" on:click={pick_personal}>
+		Personal
+	</button>
+	<button id="employment_button" on:click={pick_employment}>
+		Employment
+	</button>
+	<button id="payroll_button" on:click={pick_payroll}>
+		Payroll
+	</button>
 </div>
-  <fieldset> 
-    <legend>Personal Information</legend>
-    {#if personalVisible}
-    {#each personal_fields as field}
-      <div class="field">
-        <label for={field.field_name}>{field.header}</label>
-        <input type="text" id={field.field_name} name={field.field_name} bind:value={employee[field.field_name]}>
-      </div>
-    {/each}
-    {/if}
-  </fieldset>
-  
-  
-    {#if payrollVisible}
-      <fieldset> 
-        <legend>Employment Information</legend>
-        {#if personalVisible}
-          {#each employment_fields as field}
-            <div class="field">
-              <label for={field.field_name}>{field.header}</label>
-              <input type="text" id={field.field_name} name={field.field_name} bind:value={employee[field.field_name]}>
-            </div>
-          {/each}
-          {/if}
-              
-            </fieldset>
-          {/if}
+
+<div class="form_container">
+	{#if pages_visible.personal}
+		<div class="form_page" id="personal_info">
+			<h2>Personal Info</h2>
+			{#each personal_fields as field}
+				<Field field={field} bind:value={employee[field.field_id]}/>
+			{/each}
+		</div>
+	{/if}
+	{#if pages_visible.employment}
+  <div class="form_page" id="employment_info">
+		<h2>Employment Info</h2>
+		{#each employment_fields as field}
+			<Field field={field} bind:value={employee[field.field_id]}/>
+		{/each}
+  </div>
+	{/if}
+	{#if pages_visible.payroll}
+  <div class="form_page" id="payroll_info">
+		<h2>Payroll Info</h2>
+		{#each payroll_fields as field}
+			<Field field={field} bind:value={employee[field.field_id]}/>
+		{/each}
+  </div>
+	{/if}
+</div>
