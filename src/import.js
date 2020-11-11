1e7a0c95ed36602ac380c73ff197d3f6caa7bd98
Beth Jackson <Mbeth.ashton@gmail.com>
2020-11-11 14:30:27 -0500

    Start on Import

---
+++ src/import.js
@@ -0,0 +1,10 @@
+/* open file, return useful information from CSV. 
+designed soley for AWHN's .csv files. May not work for others. */
+
+/* strategy: 
+Right now, the only information we need is the name and the regular pay amount or the salary amount.
+
+Maybe we can take the csv, display as a table, and ask user to pick which cells apply to which information?
+
+Ok, so lets start by taking a super annoying csv and turning it into a table.
+*/
