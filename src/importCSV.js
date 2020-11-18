8aedc738e139dbdb45f8f4e01bf8992cab4510f3
William Ashton <William@AshtonFam.org>
2020-11-18 16:08:43 -0500

    Minor refactorings

--- src/importCSV.js
+++ src/importCSV.js
@@ -17,43 +17,41 @@ function parseEmployee(employeeRows) {
 }
 
 function parseRows(employee, [row, ...rest]) {
-  if (row[0].trim() !== "") {
+  if (row[0].trim()) {
     employee.name = parseColA(employee.name, row[0]);
   } 
-  if (row[1].trim() !== "") {
+  if (row[1].trim()) {
     employee.pay.push(parsePay(row[1], row[2], row[3]));
   }
-  if (row[4].trim() !== "") {
+  if (row[4].trim()) {
     employee.deductions.push(parseOther(row[4], row[5]));
   }
-  if (row[6].trim() !== "") {
+  if (row[6].trim()) {
     employee.employeeTaxes.push(parseOther(row[6], row[7]));
   }
-  if (row[8].trim() !== "") {
+  if (row[8].trim()) {
     employee.companyContributions.push(parseOther(row[8], row[9]));
   }
-  if (row[10].trim() !== "") {
+  if (row[10].trim()) {
     employee.companyTaxes.push(parseOther(row[10], row[11]));
   }
 
   if (rest.length > 0) {
     return parseRows(employee, rest);
   } else {
-    // console.log(employee);
     return employee;
   }
 }
 
 function parseColA(name_obj, cell) {
   const rows = cell.split("\n");
-  const [name, ..._rest] = rows.map(row => row.trim());
+  // const [name, ..._rest] = rows.map(row => row.trim());
+  const name = rows[0].trim();
   const name_parts = name.split(", ");
 
-  if (name_parts.length == 1) {
-    name_obj.last = name_parts[0];
-  } else if (name_parts.length == 2) {
+  name_obj.last = name_parts[0];
+  if (name_parts.length == 2) {
     name_obj.first = name_parts[1];
-    name_obj.last = name_parts[0];
   }
 
   return name_obj;
@@ -61,9 +59,9 @@ function parseColA(name_obj, cell) {
 
 function parsePay(type, hrs, amt) {
   return {
-    type: type,
-    hours: hrs,
-    amount: amt
+    type: type.trim(),
+    hours: hrs.trim(),
+    amount: amt.trim()
   };
 }
 
@@ -82,7 +80,7 @@ module.exports = function parse(rawString) {
 
   const footerRow = data.findIndex(row => row[0] === "Total");
   const allEmployeeData = data.slice(5, footerRow);
-  const footerData = data.slice(footerRow);
+  // const footerData = data.slice(footerRow);
 
   const employeeStarts =
     allEmployeeData
