0708d6b927219f9718035af08218485d3318abb8
William Ashton <William@AshtonFam.org>
2020-11-18 15:46:32 -0500

    Fix import tests

--- spec/importCSV.test.js
+++ spec/importCSV.test.js
@@ -12,73 +12,73 @@ test("Parses the payroll date", t => {
 });
 
 test("Parses the first employee", t => {
-  june_parsed = parse(june)
-  console.log(" -- Picard --")
-  picard = june_parsed.employees[0]
+  const june_parsed = parse(june);
+  // console.log(" -- Picard --");
+  const picard = june_parsed.employees[0];
 
-  t.is(picard, {
-          name: {
-            first: "Jean-Luc",
-            last: "Picard"
-          },
-          pay: [
-            {
-              type: "Regular",
-              hours: 100,
-              amount: "$2,379.00"
-            },
-            {
-              type: "Reimbursement",
-              hours: 0,
-              amount: "$4.33"
-            },
-            {
-              type: "Cell Phone Reim",
-              hours: 0,
-              amount: "$4.33"
-            }
-          ],
-          deductions: [
-            {
-              type: "401(k)",
-              amount: "$166.53"
-            }
-          ],
-          employeeTax: [
-            {
-              type: "FIT",
-              amount: "$86.25"
-            },
-            {
-              type: "SS",
-              amount: "$147.50"
-            },
-            {
-              type: "Med",
-              amount: "$34.50"
-            },
-            {
-              type: "KS PIT",
-              amount: "$76.15"
-            }
-          ],
-          company_contributions: [],
-          companyTax: [
-            {
-              type: "SS",
-              amount: "$147.50"
-            },
-            {
-              type: "Med",
-              amount: "$34.50"
-            },
-            {
-              type: "KS SUI",
-              amount: "$64.23"
-            }
-          ]
-        });
-})
+  t.deepEqual(picard, {
+    name: {
+      first: "Jean-Luc",
+      last: "Picard"
+    },
+    pay: [
+      {
+        type: "Regular",
+        hours: "100.00",
+        amount: "$2,379.00"
+      },
+      {
+        type: "Reimbursement",
+        hours: "0.00",
+        amount: "$4.33"
+      },
+      {
+        type: "Cell Phone Reim",
+        hours: "0.00",
+        amount: "$115.00"
+      }
+    ],
+    deductions: [
+      {
+        type: "401(k)",
+        amount: "$166.53"
+      }
+    ],
+    employeeTaxes: [
+      {
+        type: "FIT",
+        amount: "$86.25"
+      },
+      {
+        type: "SS",
+        amount: "$147.50"
+      },
+      {
+        type: "Med",
+        amount: "$34.50"
+      },
+      {
+        type: "KS PIT",
+        amount: "$76.15"
+      }
+    ],
+    companyContributions: [],
+    companyTaxes: [
+      {
+        type: "SS",
+        amount: "$147.50"
+      },
+      {
+        type: "Med",
+        amount: "$34.50"
+      },
+      {
+        type: "KS SUI",
+        amount: "$64.23"
+      }
+    ]
+  });
+});
 
 // t.deepEqual(parse(june), {
 //   orgName: "Starfleet WholeHealth Network",
--- src/importCSV.js
+++ src/importCSV.js
@@ -1,82 +1,77 @@
 const parseCSV = require("csv-parse/lib/sync");
 
-const default_employee = {
-  name: {
-    first: "",
-    last: ""
-  },
-  pay: [],
-  deductions: [],
-  employee_taxes: [],
-  company_contributions: [],
-  company_taxes: []
-}
-
 function parseEmployee(employeeRows) {
-  return parseRows(default_employee, employeeRows)
+  const employee = {
+    name: {
+      first: "",
+      last: ""
+    },
+    pay: [],
+    deductions: [],
+    employeeTaxes: [],
+    companyContributions: [],
+    companyTaxes: []
+  };
+
+  return parseRows(employee, employeeRows);
 }
 
 function parseRows(employee, [row, ...rest]) {
-  if (row[0] !== " " || row[0] !== "") {
-    employee.name = parseColA(employee.name, row[0])
+  if (row[0].trim() !== "") {
+    employee.name = parseColA(employee.name, row[0]);
   } 
-  if (row[1] !== " " || row[1] !== "") {
-    employee.pay.push(parsePay(row[1], row[2], row[3]))
+  if (row[1].trim() !== "") {
+    employee.pay.push(parsePay(row[1], row[2], row[3]));
   }
-  if (row[4] !== " " || row[4] !== "") {
-    employee.deductions.push(parseOther(row[4], row[5]))
+  if (row[4].trim() !== "") {
+    employee.deductions.push(parseOther(row[4], row[5]));
   }
-  if (row[6] !== " " || row[6] !== "") {
-    employee.employee_taxes.push(parseOther(row[6], row[7]))
+  if (row[6].trim() !== "") {
+    employee.employeeTaxes.push(parseOther(row[6], row[7]));
   }
-  if (row[8] !== " " || row[8] !== "") {
-    employee.company_contributions.push(parseOther(row[8], row[9]))
+  if (row[8].trim() !== "") {
+    employee.companyContributions.push(parseOther(row[8], row[9]));
   }
-  if (row[10] !== " " || row[10] !== "") {
-    employee.company_taxes.push(parseOther(row[10], row[11]))
+  if (row[10].trim() !== "") {
+    employee.companyTaxes.push(parseOther(row[10], row[11]));
   }
 
-  if (rest.length > 0){
-    return parseRows(employee, rest)
+  if (rest.length > 0) {
+    return parseRows(employee, rest);
   } else {
-    console.log(employee)
-    return employee
+    // console.log(employee);
+    return employee;
   }
 }
 
-function parseColA(name_obj, cell){
-  rows = cell.split("\n")
-  const [name, ...rest] = rows.map(row => row.trim())
-  name_parts = name.split(", ")
+function parseColA(name_obj, cell) {
+  const rows = cell.split("\n");
+  const [name, ..._rest] = rows.map(row => row.trim());
+  const name_parts = name.split(", ");
 
-  if (name_parts.length == 1){
-    name_obj.last = name_parts[0]
+  if (name_parts.length == 1) {
+    name_obj.last = name_parts[0];
   } else if (name_parts.length == 2) {
-    name_obj.first = name_parts[1],
-    name_obj.last = name_parts[0]
+    name_obj.first = name_parts[1];
+    name_obj.last = name_parts[0];
   }
 
-  return name_obj
+  return name_obj;
 }
 
-function parsePay(type, hrs, amt){
-  if (type == "" || type == " " || type == "  "){
-    return null
-  } else {
-    return {
-      type: type,
-      hours: hrs,
-      amount: amt
-    }
-  }
-  
-}
-
-function parseOther(type, amt){
+function parsePay(type, hrs, amt) {
   return {
     type: type,
+    hours: hrs,
     amount: amt
-  }
+  };
+}
+
+function parseOther(type, amt) {
+  return {
+    type: type.trim(),
+    amount: amt.trim()
+  };
 }
 
 module.exports = function parse(rawString) {
