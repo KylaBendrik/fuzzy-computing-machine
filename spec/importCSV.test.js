08a2227de93328712f082c4c5f7fe27001906656
Beth Jackson <Mbeth.ashton@gmail.com>
2020-11-18 15:17:20 -0500

    Weird issue with CSV

--- spec/importCSV.test.js
+++ spec/importCSV.test.js
@@ -11,6 +11,75 @@ test("Parses the payroll date", t => {
   t.is(parse(june).payrollDate, "Jun 13, 2020");
 });
 
+test("Parses the first employee", t => {
+  june_parsed = parse(june)
+  console.log(" -- Picard --")
+  picard = june_parsed.employees[0]
+
+  t.is(picard, {
+          name: {
+            first: "Jean-Luc",
+            last: "Picard"
+          },
+          pay: [
+            {
+              type: "Regular",
+              hours: 100,
+              amount: "$2,379.00"
+            },
+            {
+              type: "Reimbursement",
+              hours: 0,
+              amount: "$4.33"
+            },
+            {
+              type: "Cell Phone Reim",
+              hours: 0,
+              amount: "$4.33"
+            }
+          ],
+          deductions: [
+            {
+              type: "401(k)",
+              amount: "$166.53"
+            }
+          ],
+          employeeTax: [
+            {
+              type: "FIT",
+              amount: "$86.25"
+            },
+            {
+              type: "SS",
+              amount: "$147.50"
+            },
+            {
+              type: "Med",
+              amount: "$34.50"
+            },
+            {
+              type: "KS PIT",
+              amount: "$76.15"
+            }
+          ],
+          company_contributions: [],
+          companyTax: [
+            {
+              type: "SS",
+              amount: "$147.50"
+            },
+            {
+              type: "Med",
+              amount: "$34.50"
+            },
+            {
+              type: "KS SUI",
+              amount: "$64.23"
+            }
+          ]
+        });
+})
+
 // t.deepEqual(parse(june), {
 //   orgName: "Starfleet WholeHealth Network",
 //   payrollDate: "Jun 13, 2020",
--- src/importCSV.js
+++ src/importCSV.js
@@ -1,12 +1,82 @@
 const parseCSV = require("csv-parse/lib/sync");
 
-function parseEmployee(employeeRows) {
-  // do some stuff
+const default_employee = {
+  name: {
+    first: "",
+    last: ""
+  },
+  pay: [],
+  deductions: [],
+  employee_taxes: [],
+  company_contributions: [],
+  company_taxes: []
+}
 
+function parseEmployee(employeeRows) {
+  return parseRows(default_employee, employeeRows)
+}
+
+function parseRows(employee, [row, ...rest]) {
+  if (row[0] !== " ") {
+    employee.name = parseColA(employee.name, row[0])
+  } 
+  if (row[1] !== " ") {
+    employee.pay.push(parsePay(row[1], row[2], row[3]))
+  }
+  if (row[4] !== " ") {
+    employee.deductions.push(parseOther(row[4], row[5]))
+  }
+  if (row[6] !== " ") {
+    employee.employee_taxes.push(parseOther(row[6], row[7]))
+  }
+  if (row[8] !== " ") {
+    employee.company_contributions.push(parseOther(row[8], row[9]))
+  }
+  if (row[10] !== " ") {
+    employee.company_taxes.push(parseOther(row[10], row[11]))
+  }
+
+  if (rest.length > 0){
+    return parseRows(employee, rest)
+  } else {
+    console.log(employee)
+    return employee
+  }
+}
+
+function parseColA(name_obj, cell){
+  rows = cell.split("\n")
+  const [name, ...rest] = rows.map(row => row.trim())
+  name_parts = name.split(", ")
+
+  if (name_parts.length == 1){
+    name_obj.last = name_parts[0]
+  } else if (name_parts.length == 2) {
+    name_obj.first = name_parts[1],
+    name_obj.last = name_parts[0]
+  }
+
+  return name_obj
+}
+
+function parsePay(type, hrs, amt){
+  if (type == "" || type == " " || type == "  "){
+    return nil
+  } else {
+    return {
+      type: type,
+      hours: hrs,
+      amount: amt
+    }
+  }
+  
+}
+
+function parseOther(type, amt){
   return {
-    name: 'Jean-luc Picard',
-    pay: []
-  };
+    type: type,
+    amount: amt
+  }
 }
 
 module.exports = function parse(rawString) {
@@ -24,7 +94,7 @@ module.exports = function parse(rawString) {
       .map((row, id) => row[0].trim().length > 0 ? id : "blank")
       .filter(maybeId => maybeId !== "blank");
 
-  const allEmployees = [];
+  var allEmployees = [];
 
   for (let i = 0; i < employeeStarts.length; i++) {
     const start = employeeStarts[i];
@@ -33,8 +103,7 @@ module.exports = function parse(rawString) {
     // The last one works because next is undefined.
     const employeeData = allEmployeeData.slice(start, next);
 
-    const employee = parseEmployee(employeeData);
-    allEmployees.push(employee);
+    allEmployees.push(parseEmployee(employeeData));
   }
 
   return { orgName, payrollDate, employees: allEmployees };
